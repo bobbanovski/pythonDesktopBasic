95dd2fafd9e0de52328d368f66bc4c83eaa7fbc5
bobbanovski <bobbanovski@gmail.com>
2017-09-09 23:36:54 +1000

    Start of PyDownloader
    Program for downloading using URL

---
+++ pydownloader.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import *
+
+import sys
+
+class PyDownloader(QDialog):
+    def __init__: QDialog.__init__(self)
+
+
+app = QApplication(sys.argv)
+dialog = PyDownloader()
+dialog.show()
+app.exec_()
